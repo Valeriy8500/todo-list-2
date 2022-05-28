598214e4ca9714cf00a9a2a4a950490a8cb57aba
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2022-05-28 17:46:56 +0400

    Добавление функционала TodoForm и App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import NavBar from './components/navbar';
+import { TodoForm } from './components/todoForm';
 
 const App: React.FC = () => {
+  const [todos, setTodos] = React.useState([]);
+
+  const addHandler = (title: string) => {
+    console.log('add: ', title);
+  };
+
   return (
     <>
       <NavBar />
       <div className='container'>
-        <h1>Test</h1>
+        <TodoForm addHandler={addHandler} />
       </div>
     </>
   );
--- src/components/todoForm.tsx
+++ src/components/todoForm.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
-export const TodoForm: React.FC = () => {
+interface ITodoFormProps {
+    addHandler: (title: string) => void;
+}
+
+/**
+ * В компоненте представленны 2 варианта его реализации: через useState и useRef
+ */
+export const TodoForm: React.FC<ITodoFormProps> = (props) => {
+    const ref = React.useRef<HTMLInputElement>(null);
+    // const [title, setTitle] = React.useState<string>('');
+
+    const keyPressHandler = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            props.addHandler(ref.current!.value);
+            ref.current!.value = '';
+            // setTitle('');
+        }
+    };
+
+    // const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTitle(e.target.value);
+    // };
+
     return (
-        <div className='input-field'>
-            <input type="text" id='title' />
-            <label htmlFor="title" className='active'></label>
+        <div className='input-field mt2'>
+            <input
+                ref={ref}
+                onKeyPress={keyPressHandler}
+                type="text"
+                id='title'
+                placeholder='Введите название дела'
+            // onChange={changeHandler}
+            // value={title}
+            />
+            <label htmlFor="title" className='active'>
+                Введите название дела
+            </label>
         </div>
     )
 };
