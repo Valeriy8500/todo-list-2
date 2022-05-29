abd6b0547e0beb86cc73117fb01038668892863d
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2022-05-29 18:08:42 +0400

    Добавление функционала в App и TodoList

--- src/components/todoForm.tsx
+++ src/components/todoForm.tsx
@@ -1,42 +1,42 @@
 import React from 'react';
 
 interface ITodoFormProps {
-    addHandler: (title: string) => void;
+  addHandler: (title: string) => void;
 }
 
 /**
  * В компоненте представленны 2 варианта его реализации: через useState и useRef
  */
-export const TodoForm: React.FC<ITodoFormProps> = (props) => {
-    const ref = React.useRef<HTMLInputElement>(null);
-    // const [title, setTitle] = React.useState<string>('');
+export const TodoForm: React.FC<ITodoFormProps> = ({ addHandler }) => {
+  const ref = React.useRef<HTMLInputElement>(null);
+  // const [title, setTitle] = React.useState<string>('');
 
-    const keyPressHandler = (e: React.KeyboardEvent) => {
-        if (e.key === 'Enter') {
-            props.addHandler(ref.current!.value);
-            ref.current!.value = '';
-            // setTitle('');
-        }
-    };
+  const keyPressHandler = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      addHandler(ref.current!.value);
+      ref.current!.value = '';
+      // setTitle('');
+    }
+  };
 
-    // const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     setTitle(e.target.value);
-    // };
+  // const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     setTitle(e.target.value);
+  // };
 
-    return (
-        <div className='input-field mt2'>
-            <input
-                ref={ref}
-                onKeyPress={keyPressHandler}
-                type="text"
-                id='title'
-                placeholder='Введите название дела'
-            // onChange={changeHandler}
-            // value={title}
-            />
-            <label htmlFor="title" className='active'>
-                Введите название дела
-            </label>
-        </div>
-    )
+  return (
+    <div className='input-field mt2'>
+      <input
+        ref={ref}
+        onKeyPress={keyPressHandler}
+        type="text"
+        id='title'
+        placeholder='Введите название дела'
+      // onChange={changeHandler}
+      // value={title}
+      />
+      <label htmlFor="title" className='active'>
+        Введите название дела
+      </label>
+    </div>
+  )
 };
