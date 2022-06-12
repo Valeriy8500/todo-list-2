a805afcb5d3d7e6254a6eb937de7f508b532b83b
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2022-06-13 03:34:17 +0400

    add btn

--- src/components/todoForm.tsx
+++ src/components/todoForm.tsx
@@ -7,25 +7,37 @@ interface ITodoFormProps {
 export const TodoForm: React.FC<ITodoFormProps> = ({ addHandler }) => {
   const ref = React.useRef<HTMLInputElement>(null);
 
-  const keyPressHandler = (e: React.KeyboardEvent) => {
+  const keyPressHandler = (e: React.KeyboardEvent): void => {
     if (e.key === 'Enter') {
       addHandler(ref.current!.value);
       ref.current!.value = '';
     }
   };
 
+  const onAddBtn = (): void => {
+    addHandler(ref.current!.value);
+    ref.current!.value = '';
+  };
+
   return (
-    <div className='input-field mt2'>
-      <input
-        ref={ref}
-        onKeyPress={keyPressHandler}
-        type="text"
-        id='title'
-        placeholder='Введите название дела'
-      />
-      <label htmlFor="title" className='active'>
-        Введите название дела
-      </label>
+    <div className='todo-form-container'>
+      <div className='input-field mt2'>
+        <input
+          ref={ref}
+          onKeyPress={keyPressHandler}
+          type="text"
+          id='title'
+          placeholder='Введите название дела'
+        />
+        <label htmlFor="title" className='active'>
+          Введите название дела
+        </label>
+      </div>
+      <button
+        className='todo-form-btn'
+        onClick={() => onAddBtn()}>
+        Добавить
+      </button>
     </div>
   )
 };
